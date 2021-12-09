545a343052a2e6108e7aa2be27e9d63ad02ad49d
Vrezerino <parkpatrick1@gmail.com>
2021-12-09 10:01:17 +0200

    init

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ASJHDKDSK!</Text>
+      <Text>Lorem ipsum dolor</Text>
       <StatusBar style="auto" />
     </View>
   );
